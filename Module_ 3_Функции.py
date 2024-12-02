5a79c7cbb921aa25e7a67e6f27300a3405b44c21
SSasha88 <d1rus1853@gmail.com>
2024-12-02 12:44:46 +0300

    Функции, дополнил

--- Module_ 3_Функции.py
+++ Module_ 3_Функции.py
@@ -19,3 +19,40 @@ print(round(min(salary), 2), ' - минимальная зарплата в ко
 names = ['Денис', 'Антон', 'Егор', 'Катя', 'Женя']
 zipped = dict(zip(names, salary))
 print(zipped['Денис'], '- зарплата Дениса')
+
+                                                #ПРИМЕРЫ
+
+# Максимум в списке
+# Подсчёт четных чисел в списке
+# Уникальный список
+
+def find_max(list_):
+    max_ = list_[0]
+    for i in list_:
+        if i > max_:
+            max_ = i
+    return max_
+
+print('# Максимум в списке', find_max([1, 2, 1, 5, 0]))
+
+def count_even(list_):
+    counter = 0
+    for i in list_:
+        if i == 0:
+            continue
+        if i % 2 == 0:
+            counter += 1
+    return counter
+
+print('# Подсчёт четных чисел в списке',count_even([2, 2, 3, 4, 2, 1, 0]))
+
+def unique(list_):
+    new_list = []
+    for i in list_:
+        if i not in new_list:
+            new_list.append(i)
+
+    return new_list
+
+print('Уникальный список',unique([1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8]))
+
